f875bee5063f7d108830391104f8c0b0e3d399bf
Rob Squires <rob.squires.ext@bbc.co.uk>
2017-01-30 07:41:33 +0000

    added name + ticker to model

--- app/model/Stock.js
+++ app/model/Stock.js
@@ -3,7 +3,24 @@
 const {Record} = require('immutable')
 
 const Stock = Record({
+  /**
+   * name
+   * @type string
+   */
 
+  name: '',
+
+  /**
+   * ticker
+   * @type string
+   */
+  ticker: '',
+
+  /**
+   * price
+   * @type Price
+   */
+  price: null
 })
 
 module.exports = Stock
--- test/model/StockTest.js
+++ test/model/StockTest.js
@@ -8,4 +8,13 @@ describe('Stock Model', () => {
     expect(new Stock()).to.exist
     expect(new Stock()).to.instanceof(immutable.Record)
   })
+
+  it('should hold the company name', () => {
+    const stock = new Stock({ name: 'Apple'})
+    expect(stock.name).to.equal('Apple')
+  })
+  it('should hold the ticker', () => {
+    const stock = new Stock({ ticker: 'AAPL'})
+    expect(stock.ticker).to.equal('AAPL')
+  })
 })
