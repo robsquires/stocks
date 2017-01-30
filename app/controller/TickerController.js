5ea2f0bbe70912c88df02fd4bc24947ba5daf8cd
Rob Squires <rob.squires@gmail.com>
2017-01-30 22:05:58 +0000

    install react + get basic s/side rendering working

--- app/controller/TickerController.js
+++ app/controller/TickerController.js
@@ -1,8 +1,11 @@
 'use strict'
 
+const React = require('react')
+const {renderToString} = require('react-dom/server')
+
 class TickerController {
   index (req, res, next) {
-    res.render('ticker')
+    res.render('ticker', { markup: renderToString(<div>AAPL via React</div>) })
   }
 }
 
