e3938f7ce14fc6ebc96ad0128af1b4c04e2f5b60
Rob Squires <rob.squires@gmail.com>
2017-01-31 07:04:28 +0000

    created mongo source to query a mongo collection

---
+++ app/source/MongoSource.js
@@ -0,0 +1,29 @@
+'use strict'
+
+class MongoSource {
+  constructor (mongoClient, collectionName) {
+    this.mongoClient = mongoClient
+    this.collectionName = collectionName
+  }
+
+  find (query) {
+    return new Promise((resolve, reject) => {
+      this.mongoClient
+        .get()
+        .then(db => {
+          db.collection(this.collectionName)
+            .findOne(query, (err, doc) => {
+              if (err) {
+                return reject(new Error(`Could not lookup against ${this.collectionName}`))
+              }
+              resolve(doc)
+            })
+        })
+        .catch(err => {
+          reject(new Error(`Could not lookup against ${this.collectionName}`))
+        })
+    })
+  }
+}
+
+module.exports = MongoSource
---
+++ test/source/MongoSourceTest.js
@@ -0,0 +1,62 @@
+'use strict'
+
+const MongoSource = require('app/source/MongoSource')
+
+describe('Mongo Source', () => {
+  let source
+  let mongoClient
+  let mongoDb
+  let mongoCollection
+  const COLLECTION_NAME = 'collection'
+  beforeEach(() => {
+    // these need to be in reverse order for the stubbing to work correctly
+    mongoCollection = {
+      findOne: sinon.stub()
+    }
+    mongoDb = {
+      collection: sinon.stub().returns(mongoCollection)
+    }
+    mongoClient = {
+      get: sinon.stub().returns(Promise.resolve(mongoDb))
+    }
+
+    source = new MongoSource(mongoClient, COLLECTION_NAME)
+  })
+  it('should be instantiable', () => {
+    expect(source).to.exist
+  })
+
+  it('should execute the query against the collection', () => {
+    const query = { tickerCode: 'AAPL' }
+    const document = { tickerCode: 'AAPL', name: 'Apple Inc.' }
+    mongoCollection.findOne.callsArgWith(1, null, document)
+    return source.find(query)
+      .then(doc => {
+        expect(doc).to.be.ok
+        expect(doc).to.equal(document)
+      })
+  })
+  it('should return an error if the db could not be connected to', () => {
+    const query = { tickerCode: 'AAPL' }
+    mongoClient.get.returns(Promise.reject('Could not connect'))
+    return source.find(query)
+      .then(() => {
+        throw new Error('Promise should be rejected')
+      })
+      .catch(err => {
+        expect(err).to.be.ok
+      })
+  })
+
+  it('should return an error if the collection could not be queried', () => {
+    const query = { tickerCode: 'AAPL' }
+    mongoCollection.findOne.callsArgWith(1, Error('Query failed'))
+    return source.find(query)
+      .then(() => {
+        throw new Error('Promise should be rejected')
+      })
+      .catch(err => {
+        expect(err).to.be.ok
+      })
+  })
+})
