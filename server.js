00cec99631e9edb010032cdd70bd8b38e3530f13
Rob Squires <rob.squires@gmail.com>
2017-01-31 22:11:57 +0000

    added winston to aid debugging

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+global.logger = require('winston')
+global.logger.level = 'debug'
+
 // pass express runtime to our app
 require('./app')(app)
 
 // start listening
 app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`)
+  logger.info('Listening on %s', PORT)
 })
