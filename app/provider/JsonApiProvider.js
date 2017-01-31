54c393a7e2ff79cf47744f48355a529881d8e591
Rob Squires <rob.squires@gmail.com>
2017-01-31 22:50:14 +0000

    implemented json api provider

---
+++ app/provider/JsonApiProvider.js
@@ -0,0 +1,24 @@
+'use strict'
+
+class JsonApiProvider {
+  constructor (fetch, baseUrl) {
+    this.fetch = fetch
+    this.baseUrl = baseUrl
+  }
+
+  get (uri) {
+    const url = `${this.baseUrl}${uri}`
+    logger.debug('Fetching %s', url)
+    return new Promise((resolve, reject) => {
+      this.fetch(url)
+        .then(res => res.json())
+        .then(resolve)
+        .catch(err => {
+          logger.error(err)
+          reject(new Error(`Error requesting ${url}`))
+        })
+    })
+  }
+}
+
+module.exports = JsonApiProvider
